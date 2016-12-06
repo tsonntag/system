a053750bd3231578eb4d969777f00b3164fe7dda
Thomas Sonntag <git@sonntagsbox.de>
2016-12-06 16:13:34 +0100

    added ring dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.2.0"]
                  [korma "0.4.3"]
-                 [ring/ring-core "1.5.0"]
+                 [ring/ring-jetty-adaptor "1.5.0"]
                  [com.taoensso/carmine "2.11.1"]
                  [com.stuartsierra/component "0.3.1"]]
   :profiles {:dev {:source-paths ["dev"]}}
