3a995411a8b17852012818dc42a8a06740398d31
Thomas Sonntag <git@sonntagsbox.de>
2017-03-02 13:41:15 +0100

    removed defaults for excess-timeout and idle-timeout

--- jdbc/src/tsonntag/system/jdbc.clj
+++ jdbc/src/tsonntag/system/jdbc.clj
@@ -12,9 +12,7 @@
            idle-connection-test-period
            test-connection-on-checkin
            test-connection-on-checkout]
-    :or {excess-timeout (* 30 60)
-         idle-timeout (* 3 60 60)
-         minimum-pool-size 3
+    :or {minimum-pool-size 3
          maximum-pool-size 15
          test-connection-query "select 1"
          idle-connection-test-period 0
